fa9c6952fb57c0888e3479dff301b3abbabf7dea
Neha Singh <146656729+nehasingh2868@users.noreply.github.com>
2023-10-03 19:33:59 +0530

    Add files via upload

---
+++ server.js
@@ -0,0 +1,26 @@
+const express = require('express')
+const path = require('path')
+const app = express()
+const multer  = require('multer')
+const {mergePdfs}  = require('./merge')
+app.use('/static', express.static('public'))
+
+const upload = multer({ dest: 'uploads/' })
+const port = 3000
+
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, '/template/index.html'))
+})
+app.post('/merge', upload.array('pdfs', 2), function (req, res, next) {
+console.log(req.files)
+  mergePdfs(path.join(__dirname, req.files[0].path),path.join(__dirname, req.files[1].path))
+  res.redirect(`http://localhost:3000/static/merged.pdf`)
+  // res.send({data: req.files})
+  // req.files is array of `photos` files
+  // req.body will contain the text fields, if there were any
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port http://localhost:${port}`)
+})
